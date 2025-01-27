46f0d2a70032a376ef7f7066ce3fef60c31c5e45
javicoderh <justarapperguy@gmail.com>
2025-01-27 10:27:49 -0300

    test

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,13 +3,16 @@ import '../Landing/Landing.css'
 import logo from '../../assets/logo.png'
 import LogoutButton from "../usersManagement/LogoutButton";
 import LoginButton from "../usersManagement/Loginbutton";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const Header = () => {
+  const isAuthenticated = useAuth0();
 
     return (
      <header className="header">
         <img className="logo" src={logo} alt="" height={100} />
-        <h1>Studio 53</h1>        
+        <h1>Studio 53</h1>      
+        {isAuthenticated && <LogoutButton />}
     </header>
           
     );
