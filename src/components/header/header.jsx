479bf24924f58133f487646e9cbb06a58ebb5141
javicoderh <justarapperguy@gmail.com>
2025-02-13 10:46:56 -0300

    minor import issue fixed in header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import '../Landing/Landing.css'
-import logo from '../../assets/logo.png'
+import logo from '../../assets/Logo.png'
 import LogoutButton from "../usersManagement/LogoutButton";
 import LoginButton from "../usersManagement/Loginbutton";
 import { useAuth0 } from "@auth0/auth0-react";
